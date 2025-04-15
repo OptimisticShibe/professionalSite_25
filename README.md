4993a95f732baca23fa00ddc084fb02681d7bbc2
Rafiq <britishdragons123@gmail.com>
2025-04-15 17:57:38 -0500

    Content updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Rafiq Ramadan Personal Site
+
+A static-generated webpage app built with **AstroJS** and **TailwindCSS** as an excercise of design and as a simple "Portfolio Page" to host my development projects.
+
+Visit it at [rafiqramadan.com](https://rafiqramadan.com), as well as more details about its development on its [project page](https://rafiqramadan.com/projects/staticSiteDesigns).
--- src/components/TopNav/Hamburger.tsx
+++ src/components/TopNav/Hamburger.tsx
@@ -1,19 +1,16 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { navLinks } from "./NavLinks";
-import { name } from "src/constants";
 
 const Hamburger = () => {
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
-  const handleClick = () => {
-    setHamburgerOpen(!hamburgerOpen);
-  };
+  const handleClick = () => setHamburgerOpen(!hamburgerOpen);
 
   return (
     <nav className="md:hidden pt-2 bg-zinc-900 w-full flex flex-col justify-between not-prose items-center px-3">
       <div className="flex justify-between w-full">
         <a href="/">
-          <h2 className="text-2xl border-l-2 border-b-2 ml-2 pl-2 border-gold">{name}</h2>
+          <h2 className="text-2xl border-l-2 border-b-2 ml-2 pl-2 border-gold">Rafiq Ramadan</h2>
         </a>
         <button onClick={handleClick} className="hover:cursor-pointer justify-self-end border-r-2 border-t-2 border-gold p-2 mr-2">
           <span
@@ -33,17 +30,12 @@ const Hamburger = () => {
       <section className={` duration-300 ease-in-out w-full overflow-y-hidden flex justify-around mt-4 px-4  ${hamburgerOpen ? "max-h-20 pb-2" : "max-h-0"}`}>
         {navLinks.map((link, i) => {
           return (
-            <>
-              <a
-                className="hover:scale-[1.15] transition duration-300 p-2 font-bold grow text-center"
-                key={i}
-                href={link.url}
-                target={link.target ? link.target : ""}
-              >
+            <Fragment key={i}>
+              <a className="hover:scale-[1.15] transition duration-300 p-2 font-bold grow text-center" href={link.url} target={link.target ? link.target : ""}>
                 {link.label}
               </a>
               <span className="not-last:border-r-2 border-gold" />
-            </>
+            </Fragment>
           );
         })}
       </section>
