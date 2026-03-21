a219f236194cb2f57086ae832f42594c7069d602
Rafiq <urbanlizard@proton.me>
2026-03-21 14:53:47 -0500

    Add WorkOhio

--- src/pages/projects/projectEmoji.ts
+++ src/pages/projects/projectEmoji.ts
@@ -15,16 +15,15 @@ import palette from "@assets/mutantEmoji/artist_palette.svg";
 import curlPage from "@assets/mutantEmoji/curled_page.svg";
 import calendarSpiral from "@assets/mutantEmoji/calendar_spiral.svg";
 import pen from "@assets/mutantEmoji/pen.svg";
-
-// interface projectEmojiObj {
-//   top5Spotify: ImageMetadata[];
-//   staticSiteDesigns: ImageMetadata[];
-//   threeJsPortfolio: ImageMetadata[];
-// }
+// WorkOhio
+import constructionWorker from "@assets/mutantEmoji/construction_worker_o2.svg";
+import briefcase from "@assets/mutantEmoji/briefcase.svg";
+import articulatedTruck from "@assets/mutantEmoji/articulated_truck.svg";
 
 export const projectEmoji: { [name: string]: ImageMetadata[] } = {
   top5Spotify: [musicNote, crt, lp],
   staticSiteDesigns: [sun, coffee, techWorker],
   threeJsPortfolio: [rocket, computer, palette],
   irs: [curlPage, calendarSpiral, pen],
+  workOhio: [articulatedTruck, constructionWorker, briefcase],
 };
